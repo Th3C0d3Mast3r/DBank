b5226144b0efee0d7f5b9696eeebc19db358a597
Devesh <143060274+Th3C0d3Mast3r@users.noreply.github.com>
2024-07-20 18:32:58 +0530

    Update Buffers.java

--- Buffers.java
+++ Buffers.java
@@ -4,6 +4,7 @@ import java.util.stream.StreamSupport;
 
 public class Buffers {
     public void bufferFunction() {
+        //this is a basic buffer created by loop which would delay the upcoming function
         for (int i = 0; i < 10000; i++) {
             System.out.print("");
             for (int j = 0; j < 5; j++) {
